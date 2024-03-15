2dcf4998f71d202c7182b9ecf501e8e7eef37eca
Ramon <RamonFrancisco@users.noreply.github.com>
2024-03-15 18:52:58 +0000

    Finishing enpoint `/status`

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,9 +1,32 @@
+import database from "infra/database";
 import { NextResponse } from "next/server";
 
-export function GET() {
+export async function GET() {
+  const updatedAt = new Date().toISOString();
+
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbVersionResult = dbVersion.rows[0].server_version;
+
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsResult = dbMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenedConnectios = await database.query({
+    text: "SELECT count(*)::int from pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const dbOpenedConnectiosResult = dbOpenedConnectios.rows[0].count;
+
   return NextResponse.json(
     {
-      message: "Api de status",
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: dbVersionResult,
+          max_connections: parseInt(dbMaxConnectionsResult),
+          opened_connections: dbOpenedConnectiosResult,
+        },
+      },
     },
     { status: 200 },
   );
--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,6 +1,13 @@
 test("GET to '/api/v1/status' should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   const responseBody = await response.json();
-  expect(responseBody.message).toBeDefined();
+
   expect(response.status).toBe(200);
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.updated_at).toBe(parseUpdatedAt);
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
